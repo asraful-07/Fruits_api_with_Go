90829601f581d9fd56a98e72c3159aa97f172e32
asraful-07 <asraful0508@gmail.com>
2025-08-20 16:59:58 +0600

    add main go

--- cmd/main.go
+++ cmd/main.go
@@ -7,8 +7,6 @@ import (
 	"fruits-api/middleware"
 	"log"
 	"net/http"
-
-	"github.com/rs/cors"
 )
 
 func main() {
@@ -18,29 +16,22 @@ func main() {
 	manager := middleware.NewManager()
 
 	// Global Middleware (সব রিকোয়েস্টে চলবে)
-	manager.Use(middleware.Logger, middleware.Test)
+	manager.Use(
+		middleware.Logger,
+		middleware.Test,
+		middleware.CORS, 
+	)
 
 	// Sample data load
 	database.InitFruits()
 
-	// Routes (middleware ছাড়াই আগে register করি)
+	// Routes
 	mux.Handle("GET /fruits", manager.With(http.HandlerFunc(handlers.GetFruits)))
-	mux.Handle("POST /fruits",  manager.With(http.HandlerFunc(handlers.CreateFruits)))
-	mux.Handle("GET /fruits/{id}",  manager.With(http.HandlerFunc(handlers.GetById)))
-	mux.Handle("PUT /fruits-update/{id}",  manager.With(http.HandlerFunc(handlers.GetByUpdate)))
-	mux.Handle("DELETE /fruits-delete/{id}",  manager.With(http.HandlerFunc(handlers.GetByDelete)))
-	
-
-	// CORS setup
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"}, // http://localhost:5173
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Content-Type"},
-		AllowCredentials: true,
-	})
-
-	handler := c.Handler(mux)
+	mux.Handle("POST /fruits", manager.With(http.HandlerFunc(handlers.CreateFruits)))
+	mux.Handle("GET /fruits/{id}", manager.With(http.HandlerFunc(handlers.GetById)))
+	mux.Handle("PUT /fruits-update/{id}", manager.With(http.HandlerFunc(handlers.GetByUpdate)))
+	mux.Handle("DELETE /fruits-delete/{id}", manager.With(http.HandlerFunc(handlers.GetByDelete)))
 
 	fmt.Println("Server running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", handler))
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
