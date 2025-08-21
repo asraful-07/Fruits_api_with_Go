04b5e0cb024a6d1ddb36d489796670390ed2fdcb
asraful-07 <asraful0508@gmail.com>
2025-08-21 18:54:55 +0600

    add cors

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,25 +1,22 @@
 package middleware
 
 import (
-	"log"
 	"net/http"
 )
 
 // CORS Middleware
 func CORS(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// ("http://localhost:5173")
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        w.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		// OPTIONS (preflight) request handle
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
+        if r.Method == "OPTIONS" {
+            w.WriteHeader(http.StatusOK)
+            return
+        }
 
-		next.ServeHTTP(w, r)
-		log.Printf("cors is okay")
-	})
+        next.ServeHTTP(w, r)
+    })
 }
