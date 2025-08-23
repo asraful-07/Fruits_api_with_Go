38d2b6c3b23452056cdbb9d6f82a5183535c09ed
asraful-07 <asraful0508@gmail.com>
2025-08-23 11:35:27 +0600

    add just test middlware manager

--- middleware/Text.go
+++ middleware/Text.go
@@ -24,10 +24,18 @@ func (mngr *Managers) with(handler http.Handler, middlwares ...Middlewaree) http
 
 	for i := len(middlwares) - 1; i >= 0; i-- {
 		h = middlwares[i](h)
-	} 
+	}
 
 	for i := len(mngr.globalMiddlewares) -1; i >= 0; i-- {
 		h = mngr.globalMiddlewares[i](h)
 	}
 	return  h
+}
+
+func (mng *Manager) Wrap(handler http.Handler) http.Handler {
+	h := handler 
+	for i := len(mng.globalMiddlewares) - 1; i >= 0; i-- {
+		h = mng.globalMiddlewares[i](h)
+	}
+	return h
 }
