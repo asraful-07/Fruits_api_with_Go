3ae4c82d8df8d9727b6c37e5457e0f184f9e9413
asraful-07 <asraful0508@gmail.com>
2025-08-31 12:24:56 +0600

    main done

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"fruits-api/cmd"
-	"fruits-api/config"
 )
 
 func main() {
-	cfg := config.GetConfig()
-    fmt.Println("Service:", cfg.ServiceName, "Port:", cfg.HttpPort)
-	cmd.Server()
+	cmd.Serve()
 }
