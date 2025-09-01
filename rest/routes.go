50062f2c155fa6fce78266f7a4971628c4d5d37e
asraful-07 <asraful0508@gmail.com>
2025-09-01 11:23:44 +0600

    add routes

--- rest/routes.go
+++ rest/routes.go
@@ -11,6 +11,6 @@ func RegisterRoutes(mux *http.ServeMux, manager *middleware.Manager) {
 	mux.Handle("GET /fruits", manager.With(http.HandlerFunc(handlers.GetFruits)))
 	mux.Handle("POST /fruits", manager.With(http.HandlerFunc(handlers.CreateFruits)))
 	mux.Handle("GET /fruits/{id}", manager.With(http.HandlerFunc(handlers.GetById), middleware.Test))
-	mux.Handle("PUT /fruits-update/{id}", manager.With(http.HandlerFunc(handlers.GetByUpdate)))
-	mux.Handle("DELETE /fruits-delete/{id}", manager.With(http.HandlerFunc(handlers.GetByDelete)))
+	mux.Handle("PUT /fruits/{id}", manager.With(http.HandlerFunc(handlers.GetByUpdate)))
+	mux.Handle("DELETE /fruits/{id}", manager.With(http.HandlerFunc(handlers.GetByDelete)))
 }
