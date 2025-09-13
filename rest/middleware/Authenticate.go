abbdfb7e8c37fc7ffba7ee22b79894aa6199adf3
asraful-07 <asraful0508@gmail.com>
2025-09-13 15:24:16 +0600

    add authentication

---
+++ rest/middleware/Authenticate.go
@@ -0,0 +1,70 @@
+package middleware
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"fruits-api/config"
+	"net/http"
+	"strings"
+)
+
+func Authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		header := r.Header.Get("Authorization")
+
+		if header == "" {
+			http.Error(w, "UnAuthorization ff", http.StatusUnauthorized)
+			return
+		}
+
+		headerArr := strings.Split(header, " ")
+
+		if len(headerArr) != 2 {
+			http.Error(w, "UnAuthorization ee", http.StatusUnauthorized)
+			return
+		}
+
+		accessToken := headerArr[1]
+
+		tokenParts := strings.Split(accessToken, ".")
+
+		if len(tokenParts) != 3 {
+			http.Error(w, "UnAuthorization ww", http.StatusUnauthorized)
+			return
+		}
+
+		jwtHeader := tokenParts[0]
+		jwtPayload := tokenParts[1]
+		signature := tokenParts[2]
+
+		message := jwtHeader + "." + jwtPayload
+
+		cnf := config.GetConfig()
+
+		byteArrSecret := []byte(cnf.JwtSecretKey)
+		byteArrMassage := []byte(message)
+
+		h := hmac.New(sha256.New, byteArrSecret)
+		h.Write(byteArrMassage)
+
+		hash := h.Sum(nil)
+		newSignature := base64UrlEncode(hash)
+
+		if newSignature != signature {
+			http.Error(w, "UnAuthorization rr", http.StatusUnauthorized)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
+func base64UrlEncode(data []byte) string {
+	return base64.RawURLEncoding.EncodeToString(data)
+}
+
+
+// func base64UrlEncode(data []byte)  string {
+// 	return  base64.URLEncoding.WithPadding(base64.StdPadding).EncodeToString(data)
+// }
