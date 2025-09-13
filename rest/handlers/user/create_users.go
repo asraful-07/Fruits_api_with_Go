7309a2c8f93cda13559a63acfa34eb0e70a12ce1
asraful-07 <asraful0508@gmail.com>
2025-09-13 16:17:23 +0600

    add create product

---
+++ rest/handlers/user/create_users.go
@@ -0,0 +1,19 @@
+package user
+
+import (
+	"encoding/json"
+	"fruits-api/global_product"
+	"net/http"
+)
+
+func (h *Handler) CreateUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var user global_product.User 
+
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Invalid credentials", http.StatusBadRequest)
+	}
+	global_product.UserList = append(global_product.UserList, user) 
+	json.NewEncoder(w).Encode(http.StatusCreated)
+}
