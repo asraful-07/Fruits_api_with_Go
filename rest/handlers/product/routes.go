52512e32c84d520831e25556feb831bbbecea4f5
asraful-07 <asraful0508@gmail.com>
2025-09-13 16:17:09 +0600

    add routes

---
+++ rest/handlers/product/routes.go
@@ -0,0 +1,15 @@
+package product
+
+import (
+	"fruits-api/rest/middleware"
+	"net/http"
+)
+
+// RegisterRoutes সব route এখানে সেট হবে
+func (h *Handler) RegisterRoutes(mux *http.ServeMux, manager *middleware.Manager) {
+	mux.Handle("GET /fruits", manager.With(http.HandlerFunc(h.GetFruits)))
+	mux.Handle("POST /fruits", manager.With(http.HandlerFunc(h.CreateFruits), middleware.Authenticate))
+	mux.Handle("GET /fruits/{id}", manager.With(http.HandlerFunc(h.GetById), middleware.Test))
+	mux.Handle("PUT /fruits/{id}", manager.With(http.HandlerFunc(h.GetByUpdate), middleware.Authenticate))
+	mux.Handle("DELETE /fruits/{id}", manager.With(http.HandlerFunc(h.GetByDelete), middleware.Authenticate))
+}
