e311db6c4dd44acab182cbefa35c879d794aa478
asraful-07 <asraful0508@gmail.com>
2025-09-16 15:23:42 +0600

    add main.go file

--- main.go
+++ main.go
@@ -7,27 +7,3 @@ import (
 func main() {
 	cmd.Serve()
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"fruits-api/utils"
-// )
-
-// func main() {
-// 	jwt, err := utils.CreateJwt("my-secret", utils.Payload{
-// 		Sub:          "24",
-// 		FullName:     "Rahat",
-// 		Email:        "rahat@gmail.com",
-// 		IsShopeOwner: false,
-// 	})
-
-// 	if err != nil {
-// 		fmt.Println("Error:", err)
-// 		return
-// 	}
-
-// 	// JWT টোকেন টার্মিনালে দেখানোর জন্য
-// 	fmt.Println("Generated JWT:", jwt)
-// }
